a9455b391f55eefce9e769197d91137763ac2465
mori08 <mrmt0814@gmail.com>
2026-06-08 21:03:13 +0900

    Rewrite Component/ comments to follow updated comment rules #143

--- Ash2/src/Component/Hp.hpp
+++ Ash2/src/Component/Hp.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 /// @brief HP コンポーネント
-/// このコンポーネントを持つエンティティが Collider
-/// も持つとき、被弾判定の対象になる
+///
+/// このコンポーネントを持つエンティティが `Collider`
+/// も持つとき、被弾判定の対象になる。
 struct Hp {
-  /// 最大 HP
   int max;
-  /// 現在 HP
   int current;
 };
--- Ash2/src/Component/Stamina.hpp
+++ Ash2/src/Component/Stamina.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
 /// @brief スタミナコンポーネント
-/// このコンポーネントを持つエンティティのスタミナ状態を表す
 struct Stamina {
-  /// 最大スタミナ
   int max;
-  /// 現在スタミナ
   int current;
 };
